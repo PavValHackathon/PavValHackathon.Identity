52ca27bbaffee6ac91c682c8f0a8d6a82a23ecec
PavelBrest <pasha.brest.work@gmail.com>
2021-04-22 19:30:42 +0300

    started

---
+++ src/PavValHackathon.Identity/Host/IdentityServerInitializer.cs
@@ -0,0 +1,47 @@
+﻿using GodelTech.Microservices.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PavValHackathon.Identity.Domain;
+
+namespace PavValHackathon.Identity.Host
+{
+    public class IdentityServerInitializer : MicroserviceInitializerBase
+    {
+        public IdentityServerInitializer(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseIdentityServer();
+            app.UseAuthentication();
+        }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            
+            services.AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+                    options.EmitStaticAudienceClaim = true;
+                })
+                .AddAspNetIdentity<ApplicationUser>()
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = optionsBuilder => optionsBuilder.UseSqlServer(connectionString);
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = dbContextOptionsBuilder => dbContextOptionsBuilder.UseSqlServer(connectionString);
+                })
+                .AddDeveloperSigningCredential();
+        }
+    }
+}
---
+++ src/PavValHackathon.Identity/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using GodelTech.Microservices.Core;
+using GodelTech.Microservices.Core.HealthChecks;
+using GodelTech.Microservices.Core.Mvc;
+using GodelTech.Microservices.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using PavValHackathon.Identity.Host;
+
+namespace PavValHackathon.Identity
+{
+    public class Startup : MicroserviceStartup
+    {
+        public Startup(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+        
+        protected override IEnumerable<IMicroserviceInitializer> CreateInitializers()
+        {
+            yield return new DeveloperExceptionPageInitializer(Configuration);
+            yield return new HttpsInitializer(Configuration);
+
+            yield return new GenericInitializer((app, env) => app.UseRouting());
+            
+            yield return new IdentityServerInitializer(Configuration);
+            yield return new SwaggerInitializer(Configuration);
+            yield return new ApiInitializer(Configuration);
+
+            yield return new HealthCheckInitializer(Configuration);
+        }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            base.ConfigureServices(services);
+        }
+    }
+}
